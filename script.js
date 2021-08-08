b37bea121e75ffd564b7d875d4647cc0f03462ad
NAGARJUN <dasnagarjun1994@gmail.com>
2021-08-08 16:33:49 +0530

    c1

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const loadText = document.querySelector('.loading-text');
-const bg = document.querySelector('.bg');
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
 
 let load = 0;
 
-let int = setInterval(blurring,30)
+let int = setInterval(blurring, 30);
 
+function blurring() {
+  load++;
 
-function blurring(){
-    load++
+  if (load > 99) {
+    clearInterval(int);
+  }
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  // bg.style.opacity= scale(load,0,100,0,1) this will opaque the image
+  bg.style.filter = `blur(${scale(load, 0, 100, 25, 0)}px)`;
+}
 
-    if(load>99){
-        clearInterval(int);
-    }
-    loadText.innerHTML=`${load}%`;
+//took the function from here https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
